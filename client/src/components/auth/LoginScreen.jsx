4d710fbc9f6984fadd7fe05183b3895e653bdfb5
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-04-01 14:51:29 +0200

    Change error position

--- client/src/components/auth/LoginScreen.jsx
+++ client/src/components/auth/LoginScreen.jsx
@@ -17,11 +17,11 @@ const LoginScreen = () => {
   return (
     <form onSubmit={(e) => handleLogin({ username, password }, dispatch, setErrorMsg, e)} className='auth-container'>
       <div className='auth-container__wrapper'>
-        {errorMsg && <div>{errorMsg}</div>}
         <label className='auth-container__label'>Username</label>
         <input className='auth-container__input' onChange={handleInputChange} required autoComplete='off' name='username' value={username} />
         <label className='auth-container__label'>Password</label>
         <input className='auth-container__input' type='password' onChange={handleInputChange} required autoComplete='off' name='password' value={password} />
+        {errorMsg && <div>{errorMsg}</div>}
         <button className='auth-container__button' disabled={isFetching}>Login</button>
         <p className='auth-container__p'>Not having an account? <Link to='/register' className='auth-container__a'>Register Here</Link>
         </p>
