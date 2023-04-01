f780a167b1230519572ac79ef85ee5cc25a42e07
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-04-01 14:57:56 +0200

    Changed color and position of error message

--- client/src/components/auth/RegisterScreen.jsx
+++ client/src/components/auth/RegisterScreen.jsx
@@ -32,12 +32,12 @@ const RegisterScreen = () => {
 
   return (
     <form onSubmit={(e) => handleRegister(e)} className='auth-container'>
-      {confirmMessage && <div>{confirmMessage}</div>}
       <div className='auth-container__wrapper'>
         <label className='auth-container__label'>Username</label>
         <input className='auth-container__input' onChange={handleInputChange} required autoComplete='off' name='username' value={username} />
         <label className='auth-container__label'>Password</label>
         <input className='auth-container__input' onChange={handleInputChange} required type='password' autoComplete='off' name='password' value={password} />
+        {confirmMessage && <div className='auth-container__error-message'>{confirmMessage}</div>}
         <button className='auth-container__button'>Register</button>
       </div>
     </form>
