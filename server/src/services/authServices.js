d322aeff69f3b1721b876df2bb3870fa171dda0f
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-19 11:20:22 +0200

    Return user_id on register

--- server/src/services/authServices.js
+++ server/src/services/authServices.js
@@ -2,7 +2,7 @@ const pool = require('../database')
 
 const userExist = (username) => pool.query('SELECT * FROM users WHERE username=$1', [username])
 
-const createUser = (username, hashedPassword) => pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword])
+const createUser = (username, hashedPassword) => pool.query('INSERT INTO users (username, password) VALUES ($1, $2) returning user_id', [username, hashedPassword])
 
 module.exports = {
   userExist,
