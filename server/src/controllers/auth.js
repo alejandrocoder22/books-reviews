6cab13f4712a28655ef5cd18367252de396038e9
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-19 11:20:10 +0200

    Return token on register

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -2,37 +2,6 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const authServices = require('../services/authServices')
 
-const createUser = async (req, res) => {
-  const { username, password } = req.body
-
-  const userCreated = await authServices.userExist(username)
-
-  if (userCreated.rows.length > 0) {
-    res.status(400).send({ status: 'FAIL', message: 'User already exist' })
-  } else {
-    const SALT_ROUNDS = 10
-    bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
-      if (err) {
-        res.status(400).send({ status: 'FAIL', message: err })
-      }
-
-      bcrypt.hash(password, salt, (err, hashedPassword) => {
-        if (err) {
-          res.status(400).send({ status: 'FAIL', message: err })
-        }
-        authServices.createUser(username, hashedPassword)
-        res.status(201).send({ status: 'SUCESS', message: 'User ureated' })
-      })
-    })
-  }
-}
-
-const generateAccesToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_KEY, {
-    expiresIn: '2d'
-  })
-}
-
 const loginUser = async (req, res) => {
   const { username, password } = req.body
 
@@ -61,6 +30,46 @@ const loginUser = async (req, res) => {
     } else {
       res.status(403).send({ status: 'FAIL', message: 'Invalid Username Or Password' })
     }
+  })
+}
+
+const createUser = async (req, res) => {
+  const { username, password } = req.body
+
+  const userCreated = await authServices.userExist(username)
+
+  if (userCreated.rows.length > 0) {
+    return res.status(400).send({ status: 'FAIL', message: 'User already exist' })
+  } else {
+    const payload = {
+      userId: '',
+      username
+    }
+    const SALT_ROUNDS = 10
+    bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+      if (err) {
+        res.status(400).send({ status: 'FAIL', message: err })
+      }
+
+      bcrypt.hash(password, salt, async (err, hashedPassword) => {
+        if (err) {
+          res.status(400).send({ status: 'FAIL', message: err })
+        }
+
+        const userCreated = await authServices.createUser(username, hashedPassword)
+
+        payload.userId = userCreated.rows[0].user_id
+
+        const token = generateAccesToken(payload)
+        res.status(201).send({ status: 'SUCESS', message: 'User ureated', token })
+      })
+    })
+  }
+}
+
+const generateAccesToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_KEY, {
+    expiresIn: '2d'
   })
 }
 
