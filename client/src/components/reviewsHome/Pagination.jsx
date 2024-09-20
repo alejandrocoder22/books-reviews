55331a9b11be287cf95e0e0e7a84bc2180ebd81a
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-20 15:02:21 +0200

    Added pagination icons

--- client/src/components/reviewsHome/Pagination.jsx
+++ client/src/components/reviewsHome/Pagination.jsx
@@ -1,3 +1,4 @@
+import { LeftPaginateIcon, RightPaginateIcon } from '../icons/Icons'
 import PaginationButton from './PaginationButton'
 
 const Pagination = ({ numberOfPages, setPage, page }) => {
@@ -7,11 +8,23 @@ const Pagination = ({ numberOfPages, setPage, page }) => {
       {
          numberOfPages && (
            <>
-             {page !== 0 && <button className='pagination-container__button' onClick={() => setPage(page - 1)}>Previous</button>}
+             {page !== 0 &&
+              (
+                <button className='pagination-container__button' onClick={() => setPage(page + -1)}>
+                  <LeftPaginateIcon />
+                </button>
+              )}
              <PaginationButton setPage={setPage} page={page} number={page + 1} />
              {page < numberOfPages - 2 && <PaginationButton setPage={setPage} page={page} number={page + 2} />}
              {page < numberOfPages - 3 && <PaginationButton setPage={setPage} page={page} number={page + 3} />}
-             {page !== numberOfPages - 1 && <button className='pagination-container__button' onClick={() => setPage(page + 1)}>Next</button>}
+             {
+             page !== numberOfPages - 1 &&
+             (
+               <button className='pagination-container__button' onClick={() => setPage(page + 1)}>
+                 <RightPaginateIcon />
+               </button>
+             )
+             }
            </>
          )
 
